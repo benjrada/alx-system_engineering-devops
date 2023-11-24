4822f698e090015daa21ff9a79223094cf0a8299
benjrada <benjradah@gmail.com>
2023-11-24 19:23:26 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 # install flask -v 2.1.0
 
+class { 'apt':
+update => {
+frequency => 'daily',
+},
+}
+
+package { 'python3-pip':
+ensure   => installed,
+provider => apt,
+}
+
 package { 'Flask':
 ensure   => '2.1.0',
 provider => 'pip3',
 }
+
